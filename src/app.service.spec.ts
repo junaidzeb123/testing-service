4953154bcc1fb16e06d41053566c58bdf68fef6a
Junaid zeb <HP@DESKTOP-9ARMI6J>
2024-07-22 15:25:01 +0500

    removed extra thing

---
+++ src/app.service.spec.ts
@@ -0,0 +1,10 @@
+// import { Test, TestingModule } from "@nestjs/testing";
+// import { AppService } from "./app.service";
+// describe('App Service', () => {
+
+//     beforeEach(async () => {
+//         const TestingModule = Test.createTestingModule({
+//             providers : []
+//         })
+//     })
+// })
--- src/globalModules/elk/elk.service.ts
+++ src/globalModules/elk/elk.service.ts
@@ -94,222 +94,6 @@ export class ELKService {
         return (await this.searchClient.search({ index, size })).body.hits.hits.map((x: any) => x._source);
     }
 
-    async *getAllDocs(index: string, sortBy: string, sortOrder: "asc" | "desc") {
-        let response = await this.searchClient.search({
-            track_total_hits: true,
-            index: index,
-            body: {
-                sort: [{ [sortBy]: sortOrder }],
-                size: 10000
-            }
-        });
-
-        const totalResultCount = response.body.hits.total.value;
-        let yieldCount = 0;
-        yieldCount += response.body.hits.hits.length;
-        yield response.body.hits.hits.map(x => x._source);
-        while (yieldCount < totalResultCount) {
-            response = await this.searchClient.search({
-                index: this.DexTradesIndex,
-                body: {
-                    sort: [{ [sortBy]: sortOrder }],
-                    size: 10000,
-                    search_after: response.body.hits.hits[response.body.hits.hits.length - 1].sort
-                }
-            });
-            yieldCount += response.body.hits.hits.length;
-            yield response.body.hits.hits.map(x => x._source);
-        }
-    }
-
-    // get from an index by a range of a certain field  
-    async getDocsByRange(index: string, field: string, gte: number, lte: number, size: number) {
-        return (await this.searchClient.search({
-            index,
-            size,
-            body: {
-                query: {
-                    range: {
-                        [field]: {
-                            gte,
-                            lte
-                        }
-                    }
-                }
-            }
-
-        })).body.hits.hits.map((x: any) => x._source);
-    }
-
-    // remove from an index by a range of a certain field
-    async removeDocsByRange(index: string, field: string, gte: number, lte: number) {
-        return (await this.searchClient.deleteByQuery({
-            index,
-            body: {
-                query: {
-                    range: {
-                        [field]: {
-                            gte,
-                            lte
-                        }
-                    }
-                }
-            }
-
-        })).body;
-    }
-
-    // updates a single document of an index using its id value
-    async updateDocById(index: string, id: string, body: any) {
-        return (await this.searchClient.index({
-            index,
-            id,
-            body,
-        })).body;
-    }
-
-    // get all dex trades of a single trader and token
-    async getDexTradesByTraderAndToken(traderAddress: string, tokenAddress: string) {
-
-        const limiter = this.bottleneckService.getLimiter("ELK", { maxConcurrent: 10 * 1024 * 1024, minTime: 0 });
-
-        let response = await limiter.schedule({ weight: 10 * 1024 * 1024 }, async () => {
-            return await this.searchClient.search({
-                track_total_hits: true,
-                index: this.DexTradesIndex,
-                body: {
-                    query: {
-                        bool: {
-                            must: [
-                                { term: { trader_address: traderAddress } },
-                                { term: { token_address: tokenAddress } },
-                            ]
-                        }
-                    },
-                    size: 10000,
-                    sort: [{ record_index: "asc" }]
-                }
-            });
-        });
-        const totalResultCount = response.body.hits.total.value;
-        let allResults = response.body.hits.hits.map(x => x._source);
-        while (allResults.length < totalResultCount) {
-            response = await limiter.schedule({ weight: 10 * 1024 * 1024 }, async () => {
-                return await this.searchClient.search({
-                    track_total_hits: true,
-                    index: this.DexTradesIndex,
-                    body: {
-                        query: {
-                            bool: {
-                                must: [
-                                    { term: { trader_address: traderAddress } },
-                                    { term: { token_address: tokenAddress } },
-                                ]
-                            }
-                        },
-                        size: 10000,
-                        sort: [{ record_index: "asc" }],
-                        search_after: response.body.hits.hits[response.body.hits.hits.length - 1].sort
-                    }
-                });
-            });
-            allResults.push(...(response.body.hits.hits.map(x => x._source)));
-        }
-        return allResults;
-
-    }
-
-    // TODO: Work in progress
-    async getATHPriceByToken(tokenAddress: string) {
-        let response = await this.searchClient.search({
-            size: 0,
-            index: this.DexTradesIndex,
-            body: {
-                query: {
-                    term: {
-                        token_address: {
-                            value: tokenAddress
-                        }
-                    }
-                },
-                aggs: {
-                    max_price_eth: {
-                        max: {
-                            script: {
-                                source: "return doc['amount_eth'].value/doc['amount_token'].value"
-                            }
-                        }
-                    }
-                }
-            }
-        });
-        return response.body.aggregations.max_price_eth.value;
-    }
-
-    async *getAllDexTrades(_source?: string[], searchAfter?: any[]) {
-
-        let response = await this.searchClient.search({
-            track_total_hits: true,
-            index: this.DexTradesIndex,
-            _source,
-            body: {
-                sort: [{ record_index: "asc" }],
-                size: 10000,
-                search_after: searchAfter
-            }
-        });
-
-        const totalResultCount = response.body.hits.total.value;
-        let yieldCount = 0;
-        yieldCount += response.body.hits.hits.length;
-        yield response.body.hits.hits.map(x => x._source);
-        while (yieldCount < totalResultCount) {
-            response = await this.searchClient.search({
-                track_total_hits: true,
-                index: this.DexTradesIndex,
-                _source,
-                body: {
-                    sort: [{ record_index: "asc" }],
-                    size: 10000,
-                    search_after: response.body.hits.hits[response.body.hits.hits.length - 1].sort
-                }
-            });
-            yieldCount += response.body.hits.hits.length;
-            yield response.body.hits.hits.map(x => x._source);
-        }
-    }
-
-    async getAllDexPools(_source: string[]) {
-
-        let response = await this.searchClient.search({
-            track_total_hits: true,
-            index: this.DexPoolsIndex,
-            _source,
-            body: {
-                sort: [{ pool_address: "asc" }],
-                size: 10000
-            }
-        });
-
-        const totalResultCount = response.body.hits.total.value;
-        let allResults = response.body.hits.hits.map(x => x._source);
-        while (allResults.length < totalResultCount) {
-            response = await this.searchClient.search({
-                track_total_hits: true,
-                index: this.DexPoolsIndex,
-                _source,
-                body: {
-                    sort: [{ pool_address: "asc" }],
-                    size: 10000,
-                    search_after: response.body.hits.hits[response.body.hits.hits.length - 1].sort
-                }
-            });
-            console.log("latest: ", response.body.hits.hits[response.body.hits.hits.length - 1]);
-            console.log("retrieved: ", allResults.length);
-            allResults.push(...(response.body.hits.hits.map(x => x._source)));
-        }
-        return allResults;
-    }
 
     async getDocsByIds(index: string, ids: string[]) {
 
@@ -322,131 +106,13 @@ export class ELKService {
 
     }
 
-    // test
-    async scrollRetrievel(index, combinations) {
-        const fields = ["side", "trader_address", "token_address", "record_index", "timestamp", "amount_usd", "amount_token", "price_token_usd_tick_1"];
 
-        let sm = 0;
-
-        const shouldClauses = combinations.map(combination => ({
-            bool: {
-                must: [
-                    { term: { [combination.field1]: combination.value1 } },
-                    { term: { [combination.field2]: combination.value2 } }
-                ]
-            }
-        }));
-
-        const result: any[] = [];
-        let response = await this.searchClient.search({
-            track_total_hits: true,
-            index,
-            size: 10000,
-            scroll: "30s",
-            _source: fields,
-            body: {
-                query: {
-                    bool: {
-                        should: shouldClauses
-                    }
-                }
-            }
-        });
-        sm += response.body.took;
-        result.push(...(response.body.hits.hits.map((x: any) => x._source)));
-
-        while (response.body.hits.total.value > result.length) {
-
-            // get the next response if there are more quotes to fetch
-            response = await this.searchClient.scroll({
-                scroll_id: response.body._scroll_id,
-                scroll: '30s'
-            });
-            sm += response.body.took;
-            result.push(...(response.body.hits.hits.map((x: any) => x._source)));
-        }
-
-        console.log({
-            sm
-        })
-
-        return result;
-    }
-
-    // test
-    async searchAfterRetrievel(index, combinations) {
-
-        const fields = ["side", "trader_address", "token_address", "record_index", "timestamp", "amount_usd", "amount_token", "price_token_usd_tick_1"];
-
-        let sm = 0;
-
-        const shouldClauses = combinations.map(combination => ({
-            bool: {
-                must: [
-                    { term: { [combination.field1]: combination.value1 } },
-                    { term: { [combination.field2]: combination.value2 } }
-                ]
-            }
-        }));
-
-        const query = {
-            bool: {
-                should: shouldClauses
-            }
-        }
-
-        let allResults = [];
-        let hasMoreResults = true;
-        let searchAfterValues = [];
-
-        while (hasMoreResults && allResults.length < 1000000) {
-            const body = {
-                query,
-                size: 20000,
-                sort: [{ record_index: 'asc' }], // Adjust the sort field and order as needed
-                search_after: searchAfterValues,
-            };
-
-            if (searchAfterValues.length === 0) {
-                delete body.search_after;
-            }
-
-            const response = await this.searchClient.search({
-                index,
-                body,
-                _source: fields
-            });
-
-            sm += response.body.took;
-
-            const hits = response.body.hits.hits;
-
-            if (hits.length > 0) {
-                allResults = allResults.concat(hits);
-                searchAfterValues = hits[hits.length - 1].sort;
-                console.log(searchAfterValues);
-            } else {
-                hasMoreResults = false;
-            }
-        }
-
-        console.log({ sm });
-        console.log(allResults.length);
-        return allResults.map(x => x._source);
-    }
-
-    // test
-    async test_function(index, combinations) {
-        return await this.searchAfterRetrievel(index, combinations);
-    }
-
-    //test
 
     async retriveTrade(index, trader_address, token_address) {
 
         const data = await this.searchClient.search({
             index,
-            size : 10000,
+            size: 10000,
             body: {
                 query: {
                     bool: {
@@ -466,11 +132,11 @@ export class ELKService {
     // gettting random docs
     async getRandomDocs(size) {
         const seed = Date.now();
-      let result =   await this.searchClient.search({
-            index : this.DexTradesIndex,
+        let result = await this.searchClient.search({
+            index: this.DexTradesIndex,
             size: size,
             body: {
-                _source : ["token_address","trader_address"],
+                _source: ["token_address", "trader_address"],
                 query: {
                     function_score: {
                         query: {
@@ -480,7 +146,7 @@ export class ELKService {
                             {
                                 random_score: {
                                     field: "token_address",
-                                    seed:  seed.toString()
+                                    seed: seed.toString()
                                 }
                             }
                         ]
@@ -490,7 +156,7 @@ export class ELKService {
 
         })
 
-        return result.body.hits.hits.map((ele)=> ele._source );
+        return result.body.hits.hits.map((ele) => ele._source);
 
     }
 
